3b8d13d315309986604aa86c7c5fa3e4517e44b8
frpeters <francisco.peters91@gmail.com>
2017-01-26 15:31:49 -0300

    more functions

--- src/tests/wf/bitcoin/javabitcoindrpcclient/BitcoinJSONRPCClientTest.java
+++ src/tests/wf/bitcoin/javabitcoindrpcclient/BitcoinJSONRPCClientTest.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -87,10 +89,114 @@ public class BitcoinJSONRPCClientTest {
 
     @Test
     public void getReceivedByAccountTest() throws BitcoinRpcException {
-        //assertEquals(0.0, client.getReceivedByAddress(" ", 0), 0);
-        assertEquals(0.0, client.getReceivedByAddress("\"tabby\"", 0), 0);
-        assertEquals(0.0, client.getReceivedByAddress("\"tabby\"", 6), 0);
+        assertEquals(BigDecimal.valueOf(0.0), client.getReceivedByAccount("tabby", 0));
+        assertEquals(BigDecimal.valueOf(0.0), client.getReceivedByAccount("tabby", 6));
     }
+/*
+    @Test
+    public void encryptWalletTest() throws BitcoinRpcException {
+        client.encryptWallet("my pass phrase");
+    }*/
+
+/*
+    @Test
+    public void getWalletInfoTest() throws BitcoinRpcException {
+        BitcoindRpcClient.WalletInfo wf = client.getWalletInfo();
+        assertEquals(130000, wf.walletVersion());
+        assertEquals(BigDecimal.valueOf(0.0), wf.balance());
+        assertEquals(BigDecimal.valueOf(0.0), wf.unconfirmedBalance());
+        assertEquals(BigDecimal.valueOf(0.0), wf.immatureBalance());
+        assertEquals(0, wf.txCount());
+        assertEquals(1485181120, wf.keyPoolOldest());
+        assertEquals(101, wf.keyPoolSize());
+        assertEquals(0, wf.unlockedUntil());
+        assertEquals(BigDecimal.valueOf(0.0), wf.payTxFee());
+        assertEquals("46e3f9cf241f4c6fea6268788837d66e429d213c", wf.hdMasterKeyId());
+    }*/
+
+    @Test
+    public void getNetworkInfoTest() throws BitcoinRpcException {
+        BitcoindRpcClient.NetworkInfo wf = client.getNetworkInfo();
+        assertEquals(130100, wf.version());
+        assertEquals("/Satoshi:0.13.1/", wf.subversion());
+        assertEquals(70014, wf.protocolVersion());
+        assertEquals("000000000000000d", wf.localServices());
+        assertEquals(true, wf.localRelay());
+        assertEquals(0, wf.timeOffset());
+        assertEquals(8, wf.connections());
+        assertEquals(BigDecimal.valueOf(0.00001000), wf.relayFee());
+        assertEquals("", wf.warnings());
+        assertTrue(wf.localAddresses().size() == 0);
+
+        List<BitcoindRpcClient.Network> networks = wf.networks();
+        assertTrue(networks.size() == 3);
+        BitcoindRpcClient.Network net = networks.get(0);
+        assertEquals("ipv4", net.name());
+        assertEquals(false, net.limited());
+        assertEquals(true, net.reachable());
+        assertEquals("", net.proxy());
+        assertEquals(false, net.proxyRandomizeCredentials());
+
+        net = networks.get(1);
+        assertEquals("ipv6", net.name());
+        assertEquals(false, net.limited());
+        assertEquals(true, net.reachable());
+        assertEquals("", net.proxy());
+        assertEquals(false, net.proxyRandomizeCredentials());
+
+        net = networks.get(2);
+        assertEquals("onion", net.name());
+        assertEquals(true, net.limited());
+        assertEquals(false, net.reachable());
+        assertEquals("", net.proxy());
+        assertEquals(false, net.proxyRandomizeCredentials());
+
+    }
+
+    @Test
+    public void addMultiSigAddress() {
+        List<String> keys = new LinkedList<String>();
+        keys.add("1KQX9cBue11tmfjXSH3bnYtuk4hR9is3aq");
+        keys.add("187hcDcxfHktaV5g8UFxT4ABwr6pKjBRyB");
+        assertEquals("352a7HbefT2DFwM2gFmrp5PWFZyUxwCZSG", client.addMultiSigAddress(2, keys));
+    }
+
+    @Test
+    public void createMultiSigTest() {
+        List<String> keys = new LinkedList<String>();
+        keys.add("1KQX9cBue11tmfjXSH3bnYtuk4hR9is3aq");
+        keys.add("187hcDcxfHktaV5g8UFxT4ABwr6pKjBRyB");
+        BitcoindRpcClient.MultiSig ms = client.createMultiSig(2, keys);
+        assertEquals("352a7HbefT2DFwM2gFmrp5PWFZyUxwCZSG", ms.address());
+        assertEquals("5221029fc4b75fb763a54dc232c8dea005866061cbec05aa38e1ce4ace23bd5387ca1421038c4181a4d7086a62ae3d5cc541848e7ebc87368ccd22fd910964551f180ccade52ae",
+                    ms.redeemScript());
+    }
+
+    @Test
+    public void verifyChainTest() {
+        assertEquals(true, client.verifyChain());
+    }
+
+    @Test
+    public void getAddedNodeInfoTest() {
+        //client.addNode("nodoTest", "add");
+        List<BitcoindRpcClient.NodeInfo> nodeInfoList = client.getAddedNodeInfo(true, "nodoTest");
+        assertEquals("nodoTest", nodeInfoList.get(0).addedNode());
+        assertEquals(false, nodeInfoList.get(0).connected());
+    }
+
+    @Test
+    public void submitBlockTest() {
+        try {
+            client.submitBlock("mydata");
+        }
+        catch(Exception e) {
+            assertThat(e.getMessage(), is("RPC Query Failed (method: submitblock, params: [mydata], response code: 500 responseMessage Internal Server Error, response: {\"result\":null,\"error\":{\"code\":-22,\"message\":\"Block decode failed\"},\"id\":\"1\"}\n"));
+        }
+    }
+
+    //testear txout!
+
 
 
 }
